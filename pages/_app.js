c9450f129a6b80a4d6ea5313935bbe54b0d45073
Hutchii <ravodola@gmail.com>
2022-06-04 23:32:28 +0200

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import Layout from "../components/UI/Layout";
+import { GlobalStyles, lightTheme, darkTheme } from "../styles/Global";
+import { ApolloProvider } from "@apollo/client";
+import Head from "next/head";
+import { useApollo } from "../libs/apolloClient";
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [theme, setTheme] = useState(false);
+  const apolloClient = useApollo(pageProps);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ThemeProvider theme={theme ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <Layout
+          onClickHandler={() => setTheme((prevValue) => !prevValue)}
+          theme={theme}
+        >
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
